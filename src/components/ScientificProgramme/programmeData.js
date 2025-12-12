38bbb2cdda7d5b950528a81456d2a2ddb5a7d644
narender <narender.054@mymail.com>
2025-12-12 18:36:08 +0530

    added Day1 schedule

--- src/components/ScientificProgramme/programmeData.js
+++ src/components/ScientificProgramme/programmeData.js
@@ -1,363 +1,365 @@
 export const day1Data = [
   {
-    isHeader: true,
+    isHeader: false,
     cells: [
-      { content: '8.00-9.30AM', isHeaderCell: true },
-      { content: 'REGISTRATIONS AND BREAKFAST', colSpan: 3 },
+      { content: '08:30 - 09.15 AM', isHeaderCell: true },
+      { content: 'CARDIOPULMONARY BYPASS – The Heart of the Circuit <br> Precision on Pump Begins with Precision in Thought.', colSpan: 3 },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'CARDIOPULMONARY BYPASS – The Heart of the Circuit <br> Precision on Pump Begins with Precision in Thought.<br><b>MODERATORS</b> ----, ----', colSpan: 3 },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '9.00-9.30AM', isHeaderCell: true },
+      { content: '08:30 - 08:50 AM', isHeaderCell: true },
       { content: 'CPB Basics – Back to the core' },
-      { content: '----' },
-      { content: '30 MIN' },
+      { content: 'Dr. Anita Bhalla' },
+      { content: '20 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '9.30-9.45AM', isHeaderCell: true },
+      { content: '08:50 - 09:05 AM', isHeaderCell: true },
       { content: 'New Concepts – The evolving science of perfusion' },
-      { content: '----' },
+      { content: 'Dr. Anita Bhalla' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '9.45-10.05AM', isHeaderCell: true },
-      { content: '----' },
-      { content: '----' },
-      { content: '20 MIN' },
+      { content: '09:05 - 09:15 AM', isHeaderCell: true },
+      { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
+      { content: '10 MIN' },
     ],
   },
   {
     isHeader: true,
     cells: [
-      { content: '10.05-10.15AM', isHeaderCell: true },
-      { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
+      { content: '09:15 - 09:30 AM', isHeaderCell: true },
+      { content: 'TEA BREAK', colSpan: 3 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '09:30 - 10:40 AM', isHeaderCell: true },
+      { content: 'SURVIVAL SKILLS – The First 100 Days<br>“What Nobody Teaches… but Every Surgeon Must Know.” <br>', colSpan: 3 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [ 
+      { content: '09:30 - 09:45 AM', isHeaderCell: true },
+      { content: 'Mastering the 1st 100 Days of CVTS-Be the Calm in the Chaos' },
+      { content: 'Dr. Abhijeet' },
+      { content: '15 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '09:45 - 10:00 AM', isHeaderCell: true },
+      { content: 'Human Factors in Surgery – Staying safe beyond skill' },
+      { content: 'Dr. Abhijeet' },
+      { content: '15 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '10:00 - 10:10 AM', isHeaderCell: true },
+      { content: 'Managing Night Duties & Emergency Protocols- Beyond Technique Lies the Art of Judgment' },
+      { content: 'Dr. Amaresh Rao' },
       { content: '10 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '10.15-10.30AM', isHeaderCell: true },
-      { content: 'DEALING WITH QUACKS AND UNETHICAL PRACTICES' },
-      { content: 'DR SRINIVAS G' },
-      { content: '15 MIN' },
-    ],
-  },
-  {
-    isHeader: true,
-    cells: [
-      { content: '10.30-10.50AM', isHeaderCell: true },
-      { content: 'INAUGURATION', colSpan: 2 },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: true,
-    cells: [
-      { content: '10.50-11.00AM', isHeaderCell: true },
-      { content: 'TEA BREAK', colSpan: 3 },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'SURVIVAL SKILLS – The First 100 Days<br>“What Nobody Teaches… but Every Surgeon Must Know.” <br><b>MODERATORS</b>-DR ANIL KUMAR, DR CHINNASWAMY REDDY, DR TAPAN DAS', colSpan: 3 },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '11.00-11.20AM', isHeaderCell: true },
-      { content: 'Mastering the 1st 100 Days of CVTS-Be the Calm in the Chaos' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '11.20-11.40AM', isHeaderCell: true },
-      { content: 'Human Factors in Surgery – Staying safe beyond skill' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '11.40-12.00PM', isHeaderCell: true },
-      { content: 'Managing Night Duties & Emergency Protocols- Beyond Technique Lies the Art of Judgment' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '12.00-12.20PM', isHeaderCell: true },
+      { content: '10:10 - 10:20 AM', isHeaderCell: true },
       { content: 'Handover Science – “Communication That Saves Lives”' },
-      { content: '----' },
-      { content: '20 MIN' },
+      { content: 'Dr. Amaresh Rao' },
+      { content: '10 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
+      { content: '10:20 - 10:30 AM', isHeaderCell: true },
       { content: 'Medicolegal Pearls – Operate safely, document wisely' },
-      { content: '----' },
-      { content: '20 MIN' },
+      { content: 'Dr. Srinvas' },
+      { content: '10 MIN' },
     ],
   },
   {
-    isHeader: true,
+    isHeader: false,
     cells: [
-      { content: '12.20-12.30PM', isHeaderCell: true },
+      { content: '10:30 - 10:40 AM', isHeaderCell: true },
       { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
-      { content: '' },
+      { content: '10 MIN' },
     ],
   },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'DR PRACHIKAR’S ANESTHESIA SYMPOSIUM <br>“The Quiet Science That Makes Surgery Possible.”<br><b>MODERATOR</b>- DR NARMADA', colSpan: 3 },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '12.30-12.45PM', isHeaderCell: true },
-      { content: 'ANESTHESIA CONSIDERATIONS AND MONITORING ON ROUTINE CPB' },
-      { content: 'DR ARCHANA' },
-      { content: '15 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '12.45-01.00PM', isHeaderCell: true },
-      { content: 'Neonatal ICU → OR: Seamless transition' },
-      { content: '----' },
-      { content: '15 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '01.00-01.20PM', isHeaderCell: true },
-      { content: 'Post-Op ICU Management Essentials- Your Vigilance Is Someone’s Survival' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'Ventilation & Hemodynamic Algorithms <br> “Seconds to Survival: Doing the Basics Right.”' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'Ventilation & Hemodynamic Algorithms <br> “Seconds to Survival: Doing the Basics Right.”' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'Low Cardiac Output Syndrome – Prevention & rescue' },
-      { content: '----' },
-      { content: '20 MIN' },
-    ],
-  },
+
+
+
+  /* ------------------------------------------------ */
 
   {
     isHeader: true,
     cells: [
-      { content: '01.20-02.00PM', isHeaderCell: true },
-      { content: 'LUNCH', colSpan: 3 },
+      { content: 'DR PRACHIKAR’S ANESTHESIA SYMPOSIUM <br>“The Quiet Science That Makes Surgery Possible.”<br><b>MODERATOR</b>- DR NARMADA', colSpan: 4 },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'ROADMAP BEYOND M.Ch <br>“Where Your Training Takes You.”', colSpan: 3 },
+      { content: '11:30 - 11:45 AM', isHeaderCell: true },
+      { content: 'Neonatal ICU → OR: Seamless transition' },
+      { content: 'Dr. Sunil Swain' },
+      { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'WORKSHOP CORONARY <br>“Sharpen the Mind. Strengthen the Hands. Steady the Heart.”', colSpan: 3 },
+      { content: '11:45 - 12:05 PM', isHeaderCell: true },
+      { content: 'Post-Op ICU Management Essentials- Your Vigilance Is Someone’s Survival' },
+      { content: 'Dr. Poojitha' },
+      { content: '20 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'EVENING SYMPOSIUM <br>MITRAL AND AORTIC VALVE REPLACEMENT', colSpan: 3 },
+      { content: '12:05 - 12:25 PM', isHeaderCell: true },
+      { content: 'Ventilation & Hemodynamic Algorithms <br> “Seconds to Survival: Doing the Basics Right.”' },
+      { content: 'Dr. Geetha' },
+      { content: '20 MIN' },
     ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '12:25 - 12:40 PM', isHeaderCell: true },
+      { content: 'RV Dysfunction - Early pick-up, early correction' },
+      { content: 'Dr. Archana' },
+      { content: '15 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '12:40 - 12:55 PM', isHeaderCell: true },
+      { content: 'Low Cardiac Output Syndrome – Prevention & rescue' },
+      { content: 'Dr. Narmada' },
+      { content: '15 MIN' },
+    ],
+  },
+  {
+    isHeader:false,
+    cells:[
+      {content: '12:55 - 1:05 PM', isHeaderCell: true},
+      {content: 'Q &amp; A, FELICITATION', colSpan:2},
+      {content: '10 MIN'}
+    ]
   },
   {
     isHeader: true,
     cells: [
-      { content: '03.00-03.10PM', isHeaderCell: true },
-      { content: 'Q &amp; A, FELICITATION', colSpan: 3 },
+      { content: '01:05 - 02:00 PM', isHeaderCell: true },
+      { content: 'LUNCH BREAK', colSpan: 3 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '02:00 - 02:30 PM', isHeaderCell: true },
+      { content: 'ROADMAP BEYOND M.Ch <br>“Where Your Training Takes You.”'},
+      {content: 'Dr. P N Rao'},
+      {content: '30 MIN'}
+    ],
+  },
+
+  /* --------------------------------------------------------------- */
+  {
+    isHeader: 'true',
+    cells:[
+      {content: 'THORACIC SURGERY <br>"Inside the Chest: Navigating through the Mediastinum and Thorax"', colSpan:4}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      {content: '02:30 - 02:45 PM',isHeaderCell: true},
+      {content: 'Management of Thoracic Trauma'},
+      {content: 'Dr. Manjunath Bale'},
+      {content: '15 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      {content: '02:45 - 03:00 PM',isHeaderCell: true},
+      {content: 'Challenges in Tracheal Reconstruction'},
+      {content: 'Dr. Aravind Muthirevula'},
+      {content: '15 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      {content: '03:00 - 03:15 PM', isHeaderCell: true},
+      {content: 'VATS and Robotic Thoracic Surgery'},
+      {content: 'Dr. Balasubramaniam Kr'},
+      {content: '15 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      {content: '03:15 - 03:25 PM',isHeaderCell: true},
+      {content: 'Q &amp; A, FELICITATION', colSpan:2},
+      {content: '10 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '03:30 - 6:00 PM', isHeaderCell: true },
+      { content: 'THORACIC SURGERY <br>“Sharpen the Mind. Strengthen the Hands. Steady the Heart.”', colSpan: 3 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      {content: '03:30 - 04:00 PM',isHeaderCell: true},
+      {content: 'from Anatomy to Mastery: Elevating Valve Surgery.'},
+      {content: 'Dr. Thomas', colSpan: 2},
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      {content: '04:00 - 06:00 PM',isHeaderCell: true},
+      {content: 'Coronary and Valves: <br> Demonstration'},
+      {content: 'Dr. Kaladhar <br> Dr. Sai Surabhi <br> Dr. Praveen <br> Dr. Vikas', colSpan: 2},
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '06:30 - 10:00 PM', isHeaderCell: true },
+      { content: 'EVENING SYMPOSIUM <br>MITRAL AND AORTIC VALVE REPLACEMENT', colSpan: 2 },
+      {content: 'Dr. Sai Surabhi'}
     ],
   }
 ];
 
 export const day2Data = [
   {
+    isHeader: true,
+    cells: [
+      { content: 'RECENT ADVANCES & PARADIGM SHIFTS', colSpan: 4 },
+    ],
+  },
+  {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'RECENT ADVANCES & PARADIGM SHIFTS', colSpan: 2 },
+      { content: '09.00-09.15AM', isHeaderCell: true },
+      { content: 'AI-Enhanced Surgery – AI Is Not Replacing Surgeons — It’s Empowering Them ' },
+      { content: 'Dr. Amaresh Rao / Dr. PSSG' },
+      { content: '15 MIN' }, 
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '09.15-09.35 AM', isHeaderCell: true },
+      { content: 'MICS – Smaller incision, bigger impact' },
+      { content: 'Dr. Ramprassath' },
+      { content: '20 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '09:35-09:55 AM', isHeaderCell: true },
+      { content: 'Hybrid Aorta – “Less Trauma. More Precision.”' },
+      { content: 'Dr. Gopichand' },
+      { content: '20 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '09:55 - 10:15AM', isHeaderCell: true },
+      { content: 'ERAS in Cardiac Surgery – FUTURE FAST TRACK' },
+      { content: 'Dr. Ali' },
+      { content: '30 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '10:15 - 10:30 AM', isHeaderCell: true },
+      { content: 'Social Media for Surgeons – Influence. Educate. Lead.' },
+      { content: 'Dr. Srikant' },
+      { content: '30 MIN' },
+    ],
+  },
+  {
+    isHeader: true,
+    cells: [
+      { content: '10.30-10.45AM' },
+      { content: 'TEA BREAK', colSpan: 3 },
+    ],
+  },
+  {
+    isHeader: true,
+    cells: [
+      { content: 'BRIDGE TO TRANSPLANT <br> “Connecting Hope with Engineering.”',colSpan:4 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '10.45-11.00AM', isHeaderCell: true },
+      { content: 'ECMO: When seconds matter – Mastering the circuit' },
+      { content: ' ' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '09.00-09.30AM', isHeaderCell: true },
-      { content: 'AI-Enhanced Surgery – AI Is Not Replacing Surgeons — It’s Empowering Them ' },
-      { content: '----' },
-      { content: '30 MIN' }, 
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '09.30-10.00AM', isHeaderCell: true },
-      { content: 'Coronary MICS – Smaller incision, bigger impact' },
-      { content: '----' },
-      { content: '30 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'Hybrid Aorta – “Less Trauma. More Precision.”' },
-      { content: '----' },
-      { content: '30 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'ERAS in Cardiac Surgery – FUTURE FAST TRACK' },
-      { content: '----' },
-      { content: '30 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'Social Media for Surgeons – Influence. Educate. Lead.' },
-      { content: '----' },
-      { content: '30 MIN' },
-    ],
-  },
-  {
-    isHeader: true,
-    cells: [
-      { content: '10.00-10.15AM', isHeaderCell: true },
-      { content: 'Q &amp; A, FELICITATION', colSpan: 3 },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '----', isHeaderCell: true },
-      { content: 'BRIDGE TO TRANSPLANT <br> “Connecting Hope with Engineering.”' },
-      { content: 'DR SAI KIRAN' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '10.20-10.40AM', isHeaderCell: true },
-      { content: 'ECMO: When seconds matter – Mastering the circuit' },
-      { content: 'DR PN RAO' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '10.40-11.00AM', isHeaderCell: true },
+      { content: '11.00-11.15AM', isHeaderCell: true },
       { content: 'VAD Essentials -Hope Is Engineered, Not Imagined.' },
-      { content: '----' },
-      { content: '20 MIN' },
+      { content: ' ' },
+      { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '---', isHeaderCell: true },
+      { content: '11.45 - 12.00 PM', isHeaderCell: true },
       { content: 'Transplant Team Dynamics & Protocols <br> “Where Precision Meets Coordination.”' },
-      { content: '----' },
+      { content: 'Dr. John Murala' },
       { content: '20 MIN' },
     ],
   },
   {
     isHeader: true,
     cells: [
-      { content: '11.00-11.10AM', isHeaderCell: true },
-      { content: 'Q &amp; A, FELICITATION', colSpan: 3 },
-    ],
-  },
-  {
-    isHeader: true,
-    cells: [
-      { content: '11.10-11.20AM', isHeaderCell: true },
-      { content: 'TEA BREAK', colSpan: 3 },
+      {content: 'NUTRITION & RECOVERY SCIENCE <br> “Fueling Healing — ICU to Home.”',colSpan: 4},
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '----', isHeaderCell: true },
-      {
-        content: 'NUTRITION & RECOVERY SCIENCE <br> “Fueling Healing — ICU to Home.”',
-        colSpan: 2,
-      },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '12.30–1.30PM', isHeaderCell: true },
-      { content: 'Post-op Follow-Up Guidelines', colSpan: 2 },
-      { content: '---' },
+      { content: '12.00–12.15 PM', isHeaderCell: true },
+      { content: 'Post-op Follow-Up Guidelines'},
+      {content: 'Dr.Rajashekar'},
+      { content: '15 MIN' },
     ],
   },
   {

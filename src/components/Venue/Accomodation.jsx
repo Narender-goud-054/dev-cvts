086d72b1b35c6e2756f4733beee3b78cdf844488
narender <narender.054@mymail.com>
2025-12-12 18:11:38 +0530

    adding accomodation

---
+++ src/components/Venue/Accomodation.jsx
@@ -0,0 +1,9 @@
+export default function Accomodation(){
+    return (  
+    <div className="container-custom py-20">
+        <div className="bg-gradient-to-br from-blue-50 to-white rounded-2xl p-8 shadow-lg">
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">Accomodation</h1>
+            <p>To be updated</p>
+        </div>
+    </div>);
+}

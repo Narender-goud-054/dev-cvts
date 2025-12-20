b07707dffcd463d0ef5ea441054c367d737376a3
narender <narender.054@mymail.com>
2025-12-20 14:44:36 +0530

    adding faculty registration and editing some benefits

--- src/components/RegistrationPackages/RegistrationPackages.jsx
+++ src/components/RegistrationPackages/RegistrationPackages.jsx
@@ -26,6 +26,15 @@ const RegistrationPackages = () => {
     }
   ];
 
+  const faculty = {
+      title: 'Faculty',
+      price: '₹ 0',
+      category: '',
+      deadline: '',
+      type: 'student',
+      formLink: 'https://forms.gle/e6v3Gh9ZpoNutSaC7'
+  }
+
   return (
     <section id="registration" className="py-20 bg-gradient-to-b from-gray-50 to-white">
       <div className="container-custom">
@@ -76,6 +85,12 @@ const RegistrationPackages = () => {
                       <span className="text-blue-600 mr-2">•</span>
                       <span>Networking opportunities</span>
                     </li>
+                    {pkg.title !== "MBBS/UG Student" &&
+                      <li className="flex items-start">
+                        <span className="text-blue-600 mr-2">•</span>
+                        <span>Dinner Symposium</span>
+                      </li>
+                    }
                   </ul>
                 </div>
 
@@ -90,7 +105,60 @@ const RegistrationPackages = () => {
               </div>
             </div>
           ))}
-        </div>
+    
+        {/*Facutly card included in Grid after all packages*/}  
+        <div 
+              className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-2"
+            >
+          {/* Card Header */}
+          <div className="bg-gradient-to-r from-blue-900 to-blue-800 text-white p-6">
+            {/* <p className="text-sm font-semibold mb-2 opacity-90">{pkg.category}</p> */}
+            <h3 className="text-2xl font-bold mb-2">{faculty.title}</h3>
+            <div className="flex items-baseline justify-center mt-4">
+              <span className="text-5xl font-extrabold">{faculty.price.split(' ')[0]}</span>
+              <span className="text-2xl font-bold ml-2">{faculty.price.split(' ')[1]}</span>
+            </div>
+          </div>
+          {/* Card Body */}
+          <div className="p-6">
+            <div className="mb-6">        
+              <ul className="space-y-2 text-gray-600">
+                <li className="flex items-start">
+                  <span>We will be priviliged to have you</span>
+                </li>
+                <li className="flex items-start">
+                      <span className="text-blue-600 mr-2">•</span>
+                      <span>Hands-on Coronary and Valve Surgery Workshop </span>
+                    </li>
+                    <li className="flex items-start">
+                      <span className="text-blue-600 mr-2">•</span>
+                      <span>Access to all scientific sessions</span>
+                    </li>
+                    <li className="flex items-start">
+                      <span className="text-blue-600 mr-2">•</span>
+                      <span>Conference materials</span>
+                    </li>
+                    <li className="flex items-start">
+                      <span className="text-blue-600 mr-2">•</span>
+                      <span>Networking opportunities</span>
+                    </li>
+                    <li className="flex items-start">
+                        <span className="text-blue-600 mr-2">•</span>
+                        <span>Dinner Symposium</span>
+                    </li>
+              </ul>
+            </div>
+            <a 
+            href={faculty.formLink}
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="btn-primary w-full text-center"
+            >
+            Register Now
+            </a>
+          </div>
+        </div> 
+      </div>  {/* closing the Grid Div */}
 
         {/* Additional Info */}
         <div className="mt-16 bg-blue-50 rounded-xl p-8 text-center">
@@ -101,7 +169,7 @@ const RegistrationPackages = () => {
              Register now to secure your spot! Limited Slots Available.
           </p>
         </div>
-      </div>
+      </div>  {/* closing container div */}
     </section>
   );
 };

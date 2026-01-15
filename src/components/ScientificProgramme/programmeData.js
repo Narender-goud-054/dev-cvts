0e67b14d1bb90f5242ce1eeae42f2d03f1ead9fd
narender <narender.054@mymail.com>
2026-01-15 12:48:47 +0530

    15/01 scientific program

--- src/components/ScientificProgramme/programmeData.js
+++ src/components/ScientificProgramme/programmeData.js
@@ -3,185 +3,272 @@ export const day1Data = [
     isHeader: true,
     cells: [
       // { content: '08:30 - 09.15 AM', isHeaderCell: true },
-      { content: 'CARDIOPULMONARY BYPASS – The Heart of the Circuit <br> Precision on Pump Begins with Precision in Thought.', colSpan: 4 },
+      { content: 'CARDIOPULMONARY BYPASS – The Heart of the Circuit- <br> Precision on Pump Begins with Precision in Thought.', colSpan: 4 },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '08.30-08.50 AM', isHeaderCell: true },
+      { content: '08.30-08.50 AM', isHeaderCell: false },
       { content: 'CPB Basics – Back to the core' },
-      { content: '' },
+      { content: 'Mr. Saxena' },
       { content: '20 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '08.50 - 09.05 AM', isHeaderCell: true },
+      { content: '08.50-09.05 AM', isHeaderCell: false },
       { content: 'New Concepts – The evolving science of perfusion' },
-      { content: '' },
+      { content: 'Dr. Vikas Puppala' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '09.05-09.15 AM', isHeaderCell: true },
-      { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
-      { content: '10 MIN' },
+      { content: '09.05-09.20 AM', isHeaderCell: false },
+      { content: 'ECMO' },
+      { content: 'Dr. Vikram' },
+      { content: '15 MIN' },
     ],
   },
   {
-    isHeader: true,
+    isHeader: false,
     cells: [
-      { content: '09.15-09.30 AM', isHeaderCell: true },
-      { content: 'TEA BREAK', colSpan: 3 },
+      { content: '09.20-09.25 AM', isHeaderCell: false },
+      { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
+      { content: '5 MIN' },
     ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '09.20-09.45 AM', isHeaderCell: false },
+      { content: 'TEA BREAK', isHeaderCell:true,colSpan: 2 },
+      {content: '25 MIN'}
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: ''},
+    ]
   },
   {
     isHeader: true,
     cells: [
       // { content: '09:30 - 10:40 AM', isHeaderCell: true },
-      { content: 'SURVIVAL SKILLS – The First 100 Days<br>“What Nobody Teaches… but Every Surgeon Must Know.” <br>', colSpan: 4 },
+      { content: 'SURVIVAL SKILLS – The First 100 Days<br>“What Nobody Teaches… but Every Surgeon Must Know.” <br> <span style="font-weight:normal;">Moderator: Dr. Amaresh</span>', colSpan: 4 },
     ],
   },
   {
     isHeader: false,
     cells: [ 
-      { content: '09.30-09.45 AM', isHeaderCell: true },
+      { content: '09.45-10.00 AM', isHeaderCell: false },
       { content: 'Mastering the 1st 100 Days of CVTS-Be the Calm in the Chaos' },
-      { content: '' },
+      { content: 'Dr. Abhijeet' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '09.45-10.00 AM', isHeaderCell: true },
-      { content: 'Human Factors in Surgery – Staying safe beyond skill' },
-      { content: '' },
+      { content: '10.00-10.10 AM', isHeaderCell: false },
+      { content: 'Handover Science - "Communication That Saves Lives"' },
+      { content: 'Dr. Abhijeet' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '10.00-10.10 AM', isHeaderCell: true },
-      { content: 'Managing Night Duties & Emergency Protocols- Beyond Technique Lies the Art of Judgment' },
-      { content: '' },
-      { content: '10 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '10.10-10.20 AM', isHeaderCell: true },
-      { content: 'Handover Science – “Communication That Saves Lives”' },
-      { content: '' },
-      { content: '10 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '10.20-10.30 AM', isHeaderCell: true },
+      { content: '10.10-10.20 AM', isHeaderCell: false },
       { content: 'Medicolegal Pearls – Operate safely, document wisely' },
-      { content: '' },
+      { content: 'Dr. B Y Praveen' },
       { content: '10 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '10.30-10.40 AM', isHeaderCell: true },
+      { content: '10.20-10.30 AM', isHeaderCell: false },
       { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
       { content: '10 MIN' },
     ],
   },
-
-
-
+  {
+    isHeader: false,
+    cells: [
+      { content: '10.30-11.30 AM', isHeaderCell: false },
+      { content: 'INAUGURATION <br> Keynote Speech: Dr. Amaresh Rao Malempati (HOD, PROF)<br>CVTS, NIMS', isHeaderCell:true,colSpan: 2 },
+      {content: '60 MIN'}
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '11.30-12.00 PM', isHeaderCell: false },
+      { content: 'ROADMAP BEYOND M.Ch <br>“Where Your Training Takes You.”'},
+      {content: 'Dr. Lokeswar Rao Sajja'},
+      {content: '30 MIN'}
+    ],
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '12.00-12.20 PM'},
+      {content: 'PRESENT PRACTICES AND FUTURE TRENDS IN MICS'},
+      {content: 'Dr. Naresh'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '12.20-12.40 PM'},
+      {content: 'CAREER BUILDING IN THORACIC ORGAN TRANSPLANTATION'},
+      {content: 'Dr. Balasubramaniom'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '12.40-12.45 PM', isHeaderCell: false },
+      { content: 'Q &amp; A, FELICITATION', colSpan: 2 },
+      { content: '5 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '12.45-01.30 PM' },
+      { content: 'LUNCH BREAK',isHeaderCell:true, colSpan: 2 },
+      {content:'45 MIN'}
+    ],
+  },
+  {
+    isHeader:false,
+    cells:[
+      {content:''}
+    ]
+  },
   /* ------------------------------------------------ */
 
   {
     isHeader: true,
     cells: [
-      { content: 'ANESTHESIA SYMPOSIUM <br>“The Quiet Science That Makes Surgery Possible.”<br>', colSpan: 4 },
+      { content: 'Dr. PRACHIKAR\'S ANAESTHESIA SYMPOSIUM <br>“The Quiet Science That Makes Surgery Possible.”<br> <span style="font-weight:normal;">Moderator: Dr. Narmada</span>', colSpan: 4 },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '11.30-11.45 AM', isHeaderCell: true },
-      { content: 'Neonatal ICU → OR: Seamless transition' },
-      { content: '' },
+      { content: '01.30-01.45 AM', isHeaderCell: false },
+      { content: 'Management of Neonate in ICU' },
+      { content: 'Dr. Nagarajan' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '11.45-12.05 PM', isHeaderCell: true },
+      { content: '01.45-02.00 PM', isHeaderCell: false },
       { content: 'Post-Op ICU Management Essentials- Your Vigilance Is Someone’s Survival' },
-      { content: '' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '12.05-12.25 PM', isHeaderCell: true },
-      { content: 'Ventilation & Hemodynamic Algorithms <br> “Seconds to Survival: Doing the Basics Right.”' },
-      { content: '' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '12.25-12.40 PM', isHeaderCell: true },
-      { content: 'RV Dysfunction - Early pick-up, early correction' },
-      { content: '' },
+      { content: 'Dr. Poojitha' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '12.40-12.55 PM', isHeaderCell: true },
+      { content: '02.00-02.20 PM', isHeaderCell: false },
+      { content: 'Ventilation & Hemodynamic Algorithms <br> “Seconds to Survival: Doing the Basics Right.”' },
+      { content: 'Dr. Kavitha' },
+      { content: '20 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '02.20-02.35 PM', isHeaderCell: false },
+      { content: 'RV Dysfunction - Early pick-up, early correction' },
+      { content: 'Dr. Archana' },
+      { content: '15 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '02.35-02.50 PM', isHeaderCell: false },
       { content: 'Low Cardiac Output Syndrome – Prevention & rescue' },
-      { content: '' },
+      { content: 'Dr. Nitesh' },
       { content: '15 MIN' },
     ],
   },
   {
     isHeader:false,
     cells:[
-      {content: '12.55-01.05 PM', isHeaderCell: true},
+      {content: '02.50-03.00 PM', isHeaderCell: false},
       {content: 'Q &amp; A, FELICITATION', colSpan:2},
       {content: '10 MIN'}
     ]
   },
   {
     isHeader: true,
-    cells: [
-      { content: '01.05-02.00 PM', isHeaderCell: true },
-      { content: 'LUNCH BREAK', colSpan: 3 },
-    ],
+    cells:[
+      {content: 'Sharpen the Mind. Strengthen the Hands. Steady the Heart <br> <span style="font-weight:normal;">Moderator: Dr. Prateek Bhatnagar</span>', colSpan:4}
+    ]
   },
   {
-    isHeader: false,
-    cells: [
-      { content: '02.00-02.30 PM', isHeaderCell: true },
-      { content: 'ROADMAP BEYOND M.Ch <br>“Where Your Training Takes You.”'},
-      {content: ''},
+    isHeader:false,
+    cells:[
+      {content: '03:30-04.00 PM', isHeaderCell: false},
+      {content: 'How I do Coronary Anastomosis'},
+      {content: 'Dr. Shubhi'},
       {content: '30 MIN'}
-    ],
+    ]
   },
+  {
+    isHeader:false,
+    cells:[
+      {content: '04:00-06.00 PM', isHeaderCell: false},
+      {content: 'Coronary Workshop'},
+      {content: 'Dr. Tribhuvan <br> Dr. Harshitha'},
+      {content: '120 MIN'}
+    ]
+  },
+  {
+    isHeader: true,
+    cells:[
+      {content: 'DINNER SYMPOSIUM', colSpan:4}
+    ]
+  },
+  {
+    isHeader:false,
+    cells:[
+      {content: '07:00-07.40 PM', isHeaderCell: false},
+      {content: 'Mitral and Aortic Valve replacement'},
+      {content: 'Dr. Sai Surabhi'},
+      {content: '40 MIN'}
+    ]
+  },
+  {
+    isHeader:false,
+    cells:[
+      {content: '08:00-08.20 PM', isHeaderCell: false},
+      {content: 'How to excel in CARDIOTHORACIC SURGERY'},
+      {content: 'Dr. PSS GOPAL'},
+      {content: '20 MIN'}
+    ]
+  },
+]
+  
+  
 
   /* --------------------------------------------------------------- */
-  {
+ /* {
     isHeader: 'true',
     cells:[
       {content: 'THORACIC SURGERY <br>"Inside the Chest: Navigating through the Mediastinum and Thorax"', colSpan:4}
@@ -253,9 +340,88 @@ export const day1Data = [
       {content: '',colSpan:2}
     ],
   }
-];
+]; */
+
+/* -------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------- */
+
 
 export const day2Data = [
+  {
+    isHeader: true,
+    cells: [
+      { content: 'WEBINAR SESSION', colSpan: 4 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '08.30-08.50 AM', isHeaderCell: false},
+      {content: 'Is Caridac Surgery Dying ?'},
+      {content: 'Dr. John Murala'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '08.50-09.20 AM', isHeaderCell: false},
+      {content: 'Transplant Team Dynamics and Protocols'},
+      {content: 'Dr. Vydehi'},
+      {content: '30 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '09.20-09.50 AM', isHeaderCell: false},
+      {content: 'MICS and Robotic Cardiac Surgery'},
+      {content: 'Dr. Nitin Rajput'},
+      {content: '30 MIN'}
+    ]
+  },
+  
+  {
+    isHeader: true,
+    cells: [
+      { content: 'PEDIATRIC CARDIAC SURGERY', colSpan: 4 },
+    ],
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '10.00-10.20 AM', isHeaderCell: false},
+      {content: 'TOF- Timing and Choice of Surgery'},
+      {content: 'Dr. Razhane'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '10.20-10.35 AM', isHeaderCell: false},
+      {content: 'Pulmonary Hypertension'},
+      {content: 'Dr. Sudeep'},
+      {content: '15 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '10.35-10.55 AM', isHeaderCell: false},
+      {content: 'HOCM'},
+      {content: 'Dr. Sanjeev Rao'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '10.55-11.00 AM', isHeaderCell: false},
+      {content: 'Q & A, Felicitation ', colSpan:2},
+      {content: '5 MIN'}
+    ]
+  },
   {
     isHeader: true,
     cells: [
@@ -265,40 +431,114 @@ export const day2Data = [
   {
     isHeader: false,
     cells: [
-      { content: '09.00-09.15AM', isHeaderCell: true },
+      { content: '11.00-11.20 AM', isHeaderCell: false },
       { content: 'AI-Enhanced Surgery – AI Is Not Replacing Surgeons — It’s Empowering Them ' },
-      { content: '' },
+      { content: 'Dr. Arvind Muthirevula' },
       { content: '15 MIN' }, 
     ],
   },
   {
     isHeader: false,
     cells: [
-      { content: '09.15-09.35 AM', isHeaderCell: true },
-      { content: 'MICS – Smaller incision, bigger impact' },
-      { content: '' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '09.35-09.55 AM', isHeaderCell: true },
-      { content: 'Hybrid Aorta – “Less Trauma. More Precision.”' },
-      { content: '' },
-      { content: '20 MIN' },
-    ],
-  },
-  {
-    isHeader: false,
-    cells: [
-      { content: '09.55 - 10.15AM', isHeaderCell: true },
+      { content: '11.20-11.40 AM', isHeaderCell: false },
       { content: 'ERAS in Cardiac Surgery – FUTURE FAST TRACK' },
-      { content: '' },
+      { content: 'Dr. Gopichand Mannam / Dr. Saikiran' },
       { content: '20 MIN' },
     ],
   },
   {
+    isHeader: false,
+    cells: [
+      { content: '11.40-12.00 PM', isHeaderCell: false },
+      { content: 'Hybrid Aorta Surgery' },
+      { content: 'Dr. Kaladhar' },
+      { content: '20 MIN' },
+    ],
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '12.00-12.10 PM', isHeaderCell: false},
+      {content: 'Q & A, Felicitation ', colSpan: 2},
+      {content: '10 MIN'}
+    ]
+  },
+  {
+    isHeader: true,
+    cells:[
+      {content: 'THORACIC SURGERY <br> "Inside the Chest: Navigating through the Mediastinum and Thorax"', colSpan: 4}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '12.10-12.30 PM'},
+      {content: 'Learning curve in Robotic and VATS'},
+      {content: 'Dr. Manjunatahan Bale'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '12.30-12.50 PM'},
+      {content: 'Trachea - The Rising Star'},
+      {content: 'Dr. Arvind Muthirevula'},
+      {content: '20 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '12.50-01.00 PM', isHeaderCell: false},
+      {content: 'Q & A, Felicitation ', colSpan: 2},
+      {content: '10 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '01.00-01.30 PM' },
+      { content: 'LUNCH BREAK', isHeaderCell:true, colSpan: 2 },
+      { content: '30 MIN'}
+    ],
+  },
+  {
+    isHeader: false,
+    cells: [
+      { content: '' },
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '01.30-02.30 PM'},
+      {content: 'PANEL DISCUSSION <br> "When Can I Become an Independent Surgeon ?" <br> Real Questions. Honest Answers. Clear Paths'},
+      {content: 'Dr. Amaresh Rao (Moderator) <br> Dr. Abachandra'},
+      {content: '60 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '02.30-03.00 PM'},
+      {content: 'Heart Congenital Anatomy Demonstration'},
+      {content: ''},
+      {content: '30 MIN'}
+    ]
+  },
+  {
+    isHeader: false,
+    cells:[
+      {content: '03.00-06.00 PM'},
+      {content: 'Workshop: Aortic and Mitral Valve, Transplant, Lobectomy'},
+      {content: 'Dr. PSS Gopal <br> Dr. Kaladhar'},
+      {content: '180 MIN'}
+    ]
+  },
+  
+  
+  /*{
     isHeader: false,
     cells: [
       { content: '10.15-10.30 AM', isHeaderCell: true },
@@ -395,5 +635,6 @@ export const day2Data = [
       { content: 'WORKSHOP -VALVE SURGERY. <br> From Anatomy to Mastery: Elevating Valve Surgery.'},
       {content:'', colSpan:2}
     ],
-  },
+  }, */
+
 ];
